92c99483b63fb76cdcac1425dc3257d7f0af42c3
judefluhr <fluhr@purdue.edu>
2025-03-13 09:16:05 -0400

    c

--- hw08.c
+++ hw08.c
@@ -15,19 +15,81 @@
  */
 
 #ifndef INSTRUCTOR_EXPR
-enum parse_result parse_expr(struct stream *s) { return PARSE_SUCCESS; }
+enum parse_result parse_expr(struct stream *s) { 
+  if(!parse_term(s))
+  {
+    return PARSE_FAILURE;
+  }
+  return parse_expr_rest(s); 
+}
 
-enum parse_result parse_expr_rest(struct stream *s) { return PARSE_SUCCESS; }
+enum parse_result parse_expr_rest(struct stream *s) {   
+  struct token tok = peek(s);
+
+  if(tok.type == TOK_BINOP && (s[0] == '+' || s[0] == '-'))
+  {
+    next(s);
+    if(!parse_term(s))
+    {
+      return PARSE_FAILURE;
+    }
+    return parse_expr_rest(s);
+  }
+  
+  return PARSE_SUCCESS; 
+}
 #endif
 
 #ifndef INSTRUCTOR_TERM
-enum parse_result parse_term(struct stream *s) { return PARSE_SUCCESS; }
+enum parse_result parse_term(struct stream *s) { 
+  if(!parse_factor(s))
+  {
+    return PARSE_FAILURE;
+  }
+  return parse_term_rest(s); 
+}
 
-enum parse_result parse_term_rest(struct stream *s) { return PARSE_SUCCESS; }
+enum parse_result parse_term_rest(struct stream *s) { 
+  struct token tok = peek(s);
+
+  if(tok.type == TOK_BINOP && s[0] == '*')
+  {
+    next(s);
+    if(!parse_factor(s))
+    {
+      return PARSE_FAILURE;
+    }
+    return parse_term_rest(s);
+  }
+  return PARSE_SUCCESS; 
+}
 #endif
 
 #ifndef INSTRUCTOR_FACTOR
-enum parse_result parse_factor(struct stream *s) { return PARSE_FAILURE; }
+enum parse_result parse_factor(struct stream *s) { 
+  struct token tok = peek(s);
+
+  if(token.type == TOK_LITERAL)
+  {
+    next(s);
+    return PARSE_SUCCESS;
+  }
+  if(tok.type == TOK_LPAREN)
+  {
+    next(s);
+    if(!parse_expr(s))
+    {
+      return PARSE_FAILURE;
+    }
+    if(peek(s).type != TOKEN_RPAREN)
+    {
+      return PARSE_FAILURE;
+    }
+    next(s);
+    return PARSE_SUCCESS;
+  }
+  return PARSE_FAILURE; 
+}
 #endif
 
 // You do NOT need to modify this function!
